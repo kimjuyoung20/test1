c30fd9f0074388e6d80a43b89392932ac3c8c7b8
kimjuyoung20 <op45672@gmail.com>
2025-04-12 15:42:49 +0900

    Delete testp/test1 directory

--- testp/test1/src/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-
-        String javaVersion = System.getProperty("java.version");
-        System.out.println("Java Version: " + javaVersion);
-    }
-}
