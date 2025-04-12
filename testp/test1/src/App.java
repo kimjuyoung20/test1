b1cd5ba22f555c61ee3e519b6dab39b6ea02f311
kimjuyoung20 <op45672@gmail.com>
2025-04-12 13:34:32 +0900

    Add files via upload

---
+++ testp/test1/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        String javaVersion = System.getProperty("java.version");
+        System.out.println("Java Version: " + javaVersion);
+    }
+}
